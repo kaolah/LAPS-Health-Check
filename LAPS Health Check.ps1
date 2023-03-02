c2a6804bb1b6884ece0bd3673862660b170ae9a4
kaolah <35081118+kaolah@users.noreply.github.com>
2023-03-02 13:16:40 +0100

    Update LAPS Health Check.ps1

--- LAPS Health Check.ps1
+++ LAPS Health Check.ps1
@@ -1,3 +1,26 @@
+# =========================================================================
+# THIS CODE-SAMPLE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+# EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# The code sample is provided AS IS without warranty of any kind.
+# I further disclaims all implied warranties including, without 
+# limitation, any implied warranties of merchantability or of fitness for a
+# particular purpose. The entire risk arising out of the use or performance
+# of the sample and documentation remains with you. In no event shall
+# I, its authors, or anyone else involved in the creation,
+# production, or delivery of the script be liable for any damages whatsoever
+# (including, without limitation, damages for loss of business profits,
+# business interruption, loss of business information, or other pecuniary
+# loss) arising out of  the use of or inability to use the sample or
+# documentation, even if I has been advised of the possibility of
+# such damages.
+#
+# *******************
+# Author: Kamil Olah
+# *******************
+#=========================================================================
+
 param(
 [Switch]$SendMessage=$true
 )
